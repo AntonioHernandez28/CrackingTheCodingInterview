7d4eda2f4ba64728ba19dc5142d95d0e05f14b57
Antonio <a01382469@itesm.mx>
2020-11-06 02:35:00 -0600

    Problem 1.9 done

--- Arrays and Strings/Problem1_9.cpp
+++ Arrays and Strings/Problem1_9.cpp
@@ -16,6 +16,7 @@ bool isSubstring(string A, string B){
 int main(){
 
     /* Problem 1.9: Assumeyou have a method isSubstringwhich checks if one word is a substring of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one call to isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat").
+      INTERESTING TO CHECK AGAIN :) 
     */ 
 
    string A = "waterbottle"; 
